2a5b5bcb1b6b702158a9c5344f0c8821e29b005e
OUTICG01 Ivan Calderon <icalderg@emeal.nttdata.com>
2023-09-28 15:15:07 -0600

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
 import { HeroModule } from './heroes/hero.module';
+import { DbzModule } from './dbz/dbz.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,7 @@ import { HeroModule } from './heroes/hero.module';
     BrowserModule,
     CounterModule,
     HeroModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, EventEmitter,Output} from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+
+
+@Component({
+  selector: 'add-character-dbz',
+  templateUrl: './add-character.component.html',
+  styleUrls: ['./add-character.component.css']
+})
+export class AddCharacterComponent implements OnInit {
+
+  @Output()
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter();
+
+
+  public character: Character = {
+    name:'',
+    power: 0
+  };
+
+  emitCharacter():void{
+    console.log(this.character);
+    if( this.character.name.length === 0) return;
+
+    this.onNewCharacter.emit(this.character);
+
+    this.character = {name: '', power: 0};
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
